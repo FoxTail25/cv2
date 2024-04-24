8442e11247374b279b891f5dfbeef42180c507c2
Grigoriy <foxtail25@gmail.com>
2024-04-24 09:49:22 +0300

    mobil menu content

---
+++ js/data.js
@@ -0,0 +1,29 @@
+const header_menu_item = [
+    
+    {
+        name: 'hello',
+        href: '#'
+    },
+    {
+        name: 'skills',
+        href: '#'
+    },
+    {
+        name: 'expiriens',
+        href: '#'
+    },
+    {
+        name: 'education',
+        href: '#'
+    },
+    {
+        name: 'portfolio',
+        href: '#'
+    },
+    {
+        name: 'contact me',
+        href: '#'
+    },
+]
+
+exports = header_menu_item
--- js/mobil_menu.js
+++ js/mobil_menu.js
@@ -1,16 +1,29 @@
-const mobil_menu_btn = document.querySelector('.mobil_menu');
+const mobil_menu_btn = document.querySelector('.mobil_menu_btn');
+const mobil_menu_content = document.querySelector('.mobil_menu_content');
+// Открытие и закрыти меню______________________________
 let mobil_menu_open = false;
 
-mobil_menu_btn.addEventListener('click', ()=>{
-    if(mobil_menu_open) {
+mobil_menu_btn.addEventListener('click', () => {
+    if (mobil_menu_open) {
         mobil_menu_open = !mobil_menu_open
-        console.log('mobil_menu: open')
-        mobil_menu_btn.classList.add('open')
-        console.dir(mobil_menu_btn.classList)
+        mobil_menu_btn.classList.remove('open')
+        mobil_menu_content.classList.remove('open')
     } else {
         mobil_menu_open = !mobil_menu_open
-        console.log('mobil_menu: close')
-        mobil_menu_btn.classList.remove('open')
-        console.log(mobil_menu_btn)
+        mobil_menu_btn.classList.add('open')
+        mobil_menu_content.classList.add('open')
     }
 })
+//------------------------------------------------------
+// Добавление пунктов меню______________________________
+console.log(header_menu_item);
+(function createNavigation() {
+    const header_nav = document.createElement('nav');
+    header_menu_item.forEach(elem => {
+        let menu_item = document.createElement('a')
+        menu_item.href = elem.href
+        menu_item.textContent = elem.name
+        header_nav.appendChild(menu_item)
+    })
+    mobil_menu_content.appendChild(header_nav)
+}())
