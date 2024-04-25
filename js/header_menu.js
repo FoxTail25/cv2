843e372aa5af6f4111b8ba09380ed4e67f45cddd
Grigoriy <foxtail25@gmail.com>
2024-04-25 09:48:40 +0300

    header_menu

--- js/header_menu.js
+++ js/header_menu.js
@@ -1,5 +1,6 @@
 const mobil_menu_btn = document.querySelector('.mobil_menu_btn');
 const mobil_menu_content = document.querySelector('.mobil_menu_content');
+const header_content = document.querySelector('.header_content');
 // Открытие и закрыти меню______________________________
 let mobil_menu_open = false;
 
@@ -23,14 +24,27 @@ function close_mobil_menu() {
 //------------------------------------------------------
 // Добавление пунктов меню______________________________
 (function createNavigation() {
-    const header_nav = document.createElement('nav');
+    const header_nav_mobil = document.createElement('nav');
     header_menu_item.forEach(elem => {
         let menu_item = document.createElement('a')
         menu_item.href = elem.href;
         menu_item.textContent = elem.name;
         menu_item.addEventListener('click', close_mobil_menu)
-        header_nav.appendChild(menu_item)
-        header_nav.className = 'mobil_menu'
+        header_nav_mobil.appendChild(menu_item)
+        header_nav_mobil.className = 'mobil_menu'
     })
-    mobil_menu_content.appendChild(header_nav)
+    mobil_menu_content.appendChild(header_nav_mobil);
+
+
+    const header_nav_desktop = document.createElement('nav');
+    
+    header_menu_item.forEach(elem => {
+        let menu_item = document.createElement('a')
+        menu_item.href = elem.href;
+        menu_item.textContent = elem.name;
+        // menu_item.addEventListener('click', close_mobil_menu)
+        header_nav_desktop.appendChild(menu_item)
+        header_nav_desktop.className = 'desctop_menu'
+    })
+    header_content.appendChild(header_nav_desktop)
 }())
